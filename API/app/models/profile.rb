52dbcea98c9287a0d1b0d8c060fe9e894df9bb90
Esteba Arango <marranoparael31@gmail.com>
2012-04-28 12:03:57 -0500

    Creating Facebook App

--- API/app/models/profile.rb
+++ API/app/models/profile.rb
@@ -1,10 +1,26 @@
 class Profile < ActiveRecord::Base
 
-	#Associations
+	# Associations
 	belongs_to :user
   	belongs_to :employee
 
+  	validates :username, :presence => true
+	validates :email, :presence => true
+
+
+	# Callbacks
+	before_save :truncated
+
 	attr_accessible :profile_picture 
 	has_attached_file :profile_picture, :styles => { :small => "100x100>", :medium => "300x300>", :large => "400x400>" }
+
+	private
+
+	# Longer username than 40 will be truncated
+	def truncated
+		if self[:username].size > 40
+			self[:username] = self[:username][0..39]
+		end
+	end
 
 end
